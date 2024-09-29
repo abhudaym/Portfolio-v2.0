74e93b44a6ec3566144b478da1dedc7ea94dcc8c
Abhuday Mishra <52799877+abhudaym@users.noreply.github.com>
2024-09-29 21:47:15 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,8 @@ const Footer = () => {
         Abhuday Mishra
       </Link>{" "}
       &copy; {new Date().getFullYear()} <br />
-      Built using <Link href="https://nextjs.org/">NextJS</Link>
+      Built using <Link href="https://nextjs.org/">NextJS</Link> <br />
+      Inspired by <Link href="https://craftz.dog/">CraftzDog</Link>
     </Box>
   );
 };
